33230d7f60f7c11c0fcb86a6c612d7458f7d212d
liaohengfan <liaohengfan@yeah.net>
2016-05-03 16:01:49 +0800

    init Three.js Learn 2016.05.03

---
+++ src/tools/liaohengfan/InitThree.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2016/5/3.
+ */
+var InitThree=function(canvas_,w_,h_,fov_,near_,far_,anitialias){
+    var scope=this;
+
+    //基本构造
+    this.main_camera;
+    this.main_scene;
+    this.main_renderer;
+
+    this.main_scene = new THREE.Scene();
+    this.main_camera = new THREE.PerspectiveCamera(fov_, w_ / h_, near_, far_);
+    this.main_renderer = new THREE.WebGLRenderer({antialias: anitialias,canvas:canvas_});
+    this.main_renderer.setSize(w_, h_);
+    this.enterframe=null;
+    this.render=function(){
+        requestAnimationFrame(scope.render);
+
+        if(scope.enterframe instanceof Function) scope.enterframe();
+
+        scope.main_renderer.render(scope.main_scene, scope.main_camera);
+    }
+}
