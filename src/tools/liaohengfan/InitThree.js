12d108373261690b803695f89506fad1636f22f3
liaohengfan <liaohengfan@yeah.net>
2016-05-04 15:57:51 +0800

    TextGeometry Review 2016.05.04

--- src/tools/liaohengfan/InitThree.js
+++ src/tools/liaohengfan/InitThree.js
@@ -3,22 +3,18 @@
  */
 var InitThree=function(canvas_,w_,h_,fov_,near_,far_,anitialias){
     var scope=this;
-
     //基本构造
     this.main_camera;
     this.main_scene;
     this.main_renderer;
-
     this.main_scene = new THREE.Scene();
     this.main_camera = new THREE.PerspectiveCamera(fov_, w_ / h_, near_, far_);
     this.main_renderer = new THREE.WebGLRenderer({antialias: anitialias,canvas:canvas_});
     this.main_renderer.setSize(w_, h_);
-    this.enterframe=null;
+    this.enterframe=function(){};
     this.render=function(){
         requestAnimationFrame(scope.render);
-
-        if(scope.enterframe instanceof Function) scope.enterframe();
-
+        scope.enterframe();
         scope.main_renderer.render(scope.main_scene, scope.main_camera);
     }
 }
